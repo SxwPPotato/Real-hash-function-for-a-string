588af4cfc2132dca428b6507d8b96c932f02a519
Vitalya <vitalyabes.vb@gmail.com>
2023-12-30 11:22:13 +0300

    Initial commit

---
+++ Real_hash_function_for_a_string.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <math.h>
+
+int real_string_hash(std::string& str, int& p, int& n) {
+    int hash = 0;
+    for (int i = 0; i < str.size(); ++i) {
+        hash += static_cast<int> (str[i]) * pow(p,i);
+    }
+    hash = hash % n;
+    return hash;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    std::string str;
+    int p;
+    int n;
+    std::cout << "Введите p: ";
+    std::cin >> p;
+    std::cout << "Введите n: ";
+    std::cin >> n;
+    do {
+        std::cout << "\nВведите строку: ";
+        std::cin >> str;
+        std::cout << "\nНаивный хэш строки " << str << " = " << real_string_hash(str, p , n) << "\n";
+    } while (str != "exit");
+}
